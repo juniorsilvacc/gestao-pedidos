7cd52c53a03ff62b622ea8d59ff69187a3cfb616
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-05-23 00:54:54 -0300

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,32 @@
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import { router } from './routes/routes';
+import { AppError } from './config/errors/AppError';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(router);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'Error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'Error',
+      message: 'Internal server error',
+    });
+  },
+);
+
+app.listen(3333, () => {
+  console.log('Server running');
+});
