30ee49ab3059651c6cd0459b3d002b2eb5312de3
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-05-27 15:38:42 -0300

    feat: filtering products by category

---
+++ backend/src/services/products/ListProductsService.ts
@@ -0,0 +1,24 @@
+import { AppError } from '../../config/errors/AppError';
+import prismaClient from '../../prisma';
+
+interface IRequest {
+  category_id: string;
+}
+
+class ListProductsService {
+  async execute({ category_id }: IRequest) {
+    const findByCategoryId = await prismaClient.product.findMany({
+      where: {
+        category_id: category_id,
+      },
+    });
+
+    if (!findByCategoryId) {
+      throw new AppError('Products not found');
+    }
+
+    return findByCategoryId;
+  }
+}
+
+export { ListProductsService };
