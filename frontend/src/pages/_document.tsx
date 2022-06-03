1aaa3087687e1880cc9d9a052542cf4709ebd28f
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-06-03 15:49:20 -0300

    feat: settings initial

---
+++ frontend/src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+
+function Document() {
+  return (
+    <Html>
+      <Head></Head>
+      
+      <body>
+        <Main />
+        <NextScript/>
+      </body>
+    </Html>
+  )
+}
+
+export default Document
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from "next/head"
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Gestão de Pedidos - Faça seu login</title>
+      </Head>
+      <div>
+        <h1>Gestão de Pedidos</h1>
+      </div>
+    </>
+  )
+}
+
+export default Home
