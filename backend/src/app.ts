c2424f3896cd3949e49b883bc2097a7bd6dfee04
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-09-02 03:08:04 -0300

    chore: handle exceptions

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import { router } from './shared/infra/routes';
+import { exceptions } from './shared/errors/handle-exceptions';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(router);
+
+app.use(exceptions);
 
 export default app;
