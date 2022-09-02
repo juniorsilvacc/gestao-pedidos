6046d832022308e486b2ca54a5e6e348357575c9
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-09-02 19:12:44 -0300

    chore: middleware admin

---
+++ backend/src/shared/infra/middlewares/ensure-admin.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import { PostgresUsersImplementations } from '../../../modules/users/repositories/implementations/postgres-users-implementations';
+import { AppError } from '../../errors/app-error';
+
+export default async function ensureAdmin(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const { id } = request.user;
+
+  const postgresUsersImplementations = new PostgresUsersImplementations();
+  const user = await postgresUsersImplementations.findById(id);
+
+  if (!user?.isAdmin) {
+    throw new AppError("User isn't admin.");
+  }
+
+  return next();
+}
