6179bf2441983db4505c6a52797628a05afc1e37
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-09-13 02:21:18 -0300

    test: ensure to show details of a user

---
+++ backend/src/modules/users/useCases/DetailsUser/details-user-usecase.spec.ts
@@ -0,0 +1,25 @@
+import { InMemoryUsersImplementations } from '../../repositories/in-memory/in-memory-users-implementations';
+import { DetailsUserUseCase } from './details-user-usecase';
+
+let inMemoryUsersImplementations: InMemoryUsersImplementations;
+let detailsUserUseCase: DetailsUserUseCase;
+
+describe('Details User', () => {
+  beforeEach(() => {
+    inMemoryUsersImplementations = new InMemoryUsersImplementations();
+    detailsUserUseCase = new DetailsUserUseCase(inMemoryUsersImplementations);
+  });
+
+  it('should be able to detail a user', async () => {
+    const user = await inMemoryUsersImplementations.create({
+      name: 'Júnior Silva',
+      email: 'junior@hotmail.com',
+      cpf: '11122233345',
+      password: '123456',
+    });
+
+    const detail = await detailsUserUseCase.execute({ id: user.id });
+
+    expect(detail).toBe(user);
+  });
+});
