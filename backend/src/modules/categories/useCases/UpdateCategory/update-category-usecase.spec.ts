bd651a72ea15fc3edd329c619574bec3ec3b3a0c
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-09-13 13:19:25 -0300

    test: ensure don't update category if it doesn't exist

---
+++ backend/src/modules/categories/useCases/UpdateCategory/update-category-usecase.spec.ts
@@ -0,0 +1,25 @@
+import { AppError } from '../../../../shared/errors/app-error';
+import { InMemoryCategoriesImplementations } from '../../repositories/in-memory/in-memory-categories-implementations';
+import { UpdateCategoryUseCase } from './update-category-usecase';
+
+let inMemoryCategoriesImplementations: InMemoryCategoriesImplementations;
+let updateCategoryUseCase: UpdateCategoryUseCase;
+
+describe('Update Category', () => {
+  beforeEach(() => {
+    inMemoryCategoriesImplementations = new InMemoryCategoriesImplementations();
+    updateCategoryUseCase = new UpdateCategoryUseCase(
+      inMemoryCategoriesImplementations,
+    );
+  });
+
+  it('should not be able to update a user does not exists', async () => {
+    expect(
+      updateCategoryUseCase.execute({
+        id: 'b6a3582a-e54d-444b-bf70-3affd4fb28fe',
+        name: 'non-existing category',
+        description: 'Category Description',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
