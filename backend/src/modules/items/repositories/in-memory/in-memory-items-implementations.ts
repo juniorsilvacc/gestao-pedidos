ea499b61fcc81508b838107c671512accca8c461
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-09-14 02:28:53 -0300

    chore: in memory items

---
+++ backend/src/modules/items/repositories/in-memory/in-memory-items-implementations.ts
@@ -0,0 +1,39 @@
+import { ICreateItemDTO } from '../../dtos/create-item-dto';
+import { Item } from '../../models/item';
+import { IItemsRepository } from '../items-repository';
+
+class InMemoryItemsImplementations implements IItemsRepository {
+  private items: Item[] = [];
+
+  async create({
+    amount,
+    order_id,
+    product_id,
+  }: ICreateItemDTO): Promise<Item> {
+    const item = new Item();
+
+    Object.assign(item, { amount, order_id, product_id });
+
+    this.items.push(item);
+
+    return item;
+  }
+
+  async removeItem(id: string): Promise<void> {
+    this.items.find(item => item.id === id);
+  }
+
+  async findById(id: string): Promise<Item | null> {
+    const item = this.items.find(item => item.id === id);
+
+    return item || null;
+  }
+
+  async findDetail(order_id: string): Promise<Item[]> {
+    this.items.find(item => item.id === order_id);
+
+    return [];
+  }
+}
+
+export { InMemoryItemsImplementations };
