ce55e6e40e50000eaab50faf58b006e7019756ee
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-09-14 02:08:06 -0300

    test: ensure close order

---
+++ backend/src/modules/orders/useCases/CloseOrder/close-order-usecase.spec.ts
@@ -0,0 +1,36 @@
+import { AppError } from '../../../../shared/errors/app-error';
+import { InMemoryOrdersImplementations } from '../../repositories/in-memory/in-memory-orders-implementations';
+import { CloseOrderUseCase } from './close-order-usecase';
+
+let inMemoryOrdersImplementations: InMemoryOrdersImplementations;
+let closeOrderUseCase: CloseOrderUseCase;
+
+describe('Close Order', () => {
+  beforeEach(() => {
+    inMemoryOrdersImplementations = new InMemoryOrdersImplementations();
+    closeOrderUseCase = new CloseOrderUseCase(inMemoryOrdersImplementations);
+  });
+
+  it('should not be able to close a order does not exists', async () => {
+    expect(
+      closeOrderUseCase.execute({
+        order_id: 'non-existent',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should close a order', async () => {
+    const order = await inMemoryOrdersImplementations.create({
+      name: 'Name Test',
+      table: 10,
+      status: false,
+      draft: false,
+    });
+
+    const closeOrder = await closeOrderUseCase.execute({
+      order_id: order.id,
+    });
+
+    expect(closeOrder).toBeUndefined();
+  });
+});
