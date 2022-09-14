06209783ab886343e1341cf7dd79059fd289b679
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-09-14 03:39:02 -0300

    test: ensure remove item

--- backend/src/modules/items/useCases/RemoveItem/remove-item-usecase.spec.ts
+++ backend/src/modules/items/useCases/RemoveItem/remove-item-usecase.spec.ts
@@ -18,4 +18,18 @@ describe('Remove Item', () => {
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
+
+  it('should remove a item', async () => {
+    const item = await inMemoryItemsImplementations.create({
+      amount: 2,
+      order_id: '7f7eab8f-e1ed-47d8-8cba-dc95bd9bf035',
+      product_id: 'a9814566-901a-4a33-8615-5b2bcf8c80af',
+    });
+
+    const removeItem = await removeItemUseCase.execute({
+      id: item.id,
+    });
+
+    expect(removeItem).toBeUndefined();
+  });
 });
